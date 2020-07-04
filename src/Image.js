e1152b2769eb37d28fe532527a934bc7cdc91684
David Kern <david33belle@gmail.com>
2020-07-04 12:33:23 +0300

    initial commit

--- src/Image.js
+++ src/Image.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+const mystyle = {
+  width: "100%"
+};
+
 const Img = ({ src = "", alt = "", classStyle = "" }) => (
-  <img src={src} alt={alt} className={classStyle} />
+  <img src={src} alt={alt} className={classStyle} style={mystyle} />
 );
 
 Img.propTypes = {
