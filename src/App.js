85f6f81e3249d9d78941753c2732226dc08ef980
David Kern <david33belle@gmail.com>
2020-07-07 11:33:22 +0300

    :bug: Bugfix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import "./styles.css";
 
 import LazyImage from "./LazyImage";
 
-const Image =
-  "https://i.ibb.co/DtbLVMN/Purple-and-White-Math-Tutor-Bordered-Linked-In-Banner.png";
-
+const Image = "https://i.ibb.co/DtbLVMN/Purple-and-White-Math-Tutor-Bordered-Linked-In-Banner.png";
 const ErrorImage = "https://i.ibb.co/5Ykzw98/Lighter-District.png";
-
 const BrokenImage = "https://i.ibb.co/5Ykzw98/Ligh-District.png";
 
 export default function App() {
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import GalleryContainer from "./containers/GalleryContainer";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <GalleryContainer />
   </React.StrictMode>,
   rootElement
 );
